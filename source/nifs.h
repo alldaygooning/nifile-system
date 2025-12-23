624cf637f8f0ebf9f3635fb2c4ff50af95414787
alldaygooning <nikich1402@mail.ru>
2025-12-23 12:20:26 +0300

    Added module versioning

--- source/nifs.h
+++ source/nifs.h
@@ -10,6 +10,7 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("allgaygooning");
 MODULE_DESCRIPTION("G-E-N-I-U-S file system");
+MODULE_VERSION("pre-beta.snapshot.a1.0.1");
 
 extern struct file_system_type nifs_fs_type; // NOLINT
 
